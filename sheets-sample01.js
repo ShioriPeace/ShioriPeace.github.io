324eb1921d16c6fbddfb73bb269294c0544c5787
ShioriPeace <m0116225f2@edu.teu.ac.jp>
2018-11-21 18:18:34 +0900

    課題２まで

--- sheets-sample01.js
+++ sheets-sample01.js
@@ -1,9 +1,3 @@
-/*fetch("https://sheets.googleapis.com/v4/spreadsheets/1vvNBYDw8iaPidm7Z4-UtGLY8-Y2Yf5hzu2v5v7m1vHA/values/sheet1!A1:C6?key=AIzaSyDGu3-jXAEgxc9ywdeeFWXw9e8QGE3uk2c").then(
-    response => {
-        return response.json();
-    }
-).*/ //先生
-
 fetch("https://sheets.googleapis.com/v4/spreadsheets/1bULwtvSec8ZKUiJZ8qvPVgKpX0wGKKQXk7wekoP4lbo/values/data?key=AIzaSyAHmMpbMqePjuQXzThfIjZ4zAFkwaFHxTE").then(
     response => {
         return response.json();
@@ -15,10 +9,10 @@ fetch("https://sheets.googleapis.com/v4/spreadsheets/1bULwtvSec8ZKUiJZ8qvPVgKpX0
         //ここに何か書く
 
         json.values.forEach(row =>{ //jsonの中のvalueというところに入っているのを全て取り出す→その中のrowという配列の中を取り出す
-          html +=  `<li><${row[0]},${row[1]}.${row[2]}</li>`
+            html +=  `<li>${row[0]}</li>`
         });//ここはfunction row
 
-        html = "</ul>";
+        html += "</ul>";
         document.querySelector("#result").innerHTML = html;
     }
 );
