dbc92af130cd344e6ef07f365ed3af703144c672
ShioriPeace <m0116225f2@edu.teu.ac.jp>
2018-11-21 16:09:42 +0900

    大まかな配置完了 →文字が左に行かないトラブルあり

---
+++ array_tweet.js
@@ -0,0 +1,12 @@
+let tweetsHTML ="<ul>";
+
+let appendTweet = function(tweet) {
+    tweetsHTML += `<li><div class="ImgBox"><img src=${tweet.avatar} width = "100%"></div> <div class="TextBox"><b>${tweet.name}</b>${tweet.message} ${tweet.tweetedAt}</div></li>`;
+
+};
+
+
+tweets.forEach(appendTweet);
+
+tweetsHTML += "</ul>";
+document.querySelector("#answer").innerHTML = tweetsHTML;
