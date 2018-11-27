bc229b04b669468b3d96bcdcaaa169a4a071b3d4
ShioriPeace <m0116225f2@edu.teu.ac.jp>
2018-11-27 11:46:30 +0900

    allボタン以外全てのボタンが作用

--- array_tweet.js
+++ array_tweet.js
@@ -1,7 +1,9 @@
 let tweetsHTML ="<ul>";
 
+
 let appendTweet = function(tweet) {
-    tweetsHTML += `<li><div class="ImgBox"><img src=${tweet.avatar} alt="" style="width: 100%"></div> <div class="TextBox"><b>${tweet.name}</b>${tweet.message} ${tweet.tweetedAt}</div></li>`;
+    tweetsHTML += `<li><div class="ImgBox"><img src=${tweet.avatar} alt="" style="width: 100%"></div> <div class="TextBox"><b>${tweet.name}</b><br>${tweet.message} <br>${tweet.tweetedAt}</div></li>`;
+
 
 };
 
@@ -9,9 +11,54 @@ let appendTweet = function(tweet) {
 tweets.forEach(appendTweet);
 
 tweetsHTML += "</ul>";
+
 document.querySelector("#answer").innerHTML = tweetsHTML;
 
-function onClickButton(){
-    
+
+
+function onClickButtonSaburo() {
+    let tweetsHTML ="<ul>";
+    let appendTweet = function (tweet) {
+        if(tweet.name === '三郎BOT'){
+            tweetsHTML +=  `<li><div class="ImgBox"><img src=${tweet.avatar} alt="" style="width: 100%"></div> <div class="TextBox"><b>${tweet.name}</b><br>${tweet.message} <br>${tweet.tweetedAt}</div></li>`;
+
+        }
+
+    };
+
+    tweets.forEach(appendTweet);
+    tweetsHTML += "</ul>";
+    document.querySelector("#answer").innerHTML = tweetsHTML;
+
+   /* let Saburo = tweets.filter(x => x.name === '三郎BOT');
+    document.querySelectorAll("#answer").innerHTML = Saburo;
+    console.log(Saburo);*/
+}
+
+function onClickButton() {
+    let tweetsHTML ="<ul>";
+    let appendTweet = function (tweet) {
+        if(tweet.name === '次郎'){
+            tweetsHTML +=  `<li><div class="ImgBox"><img src=${tweet.avatar} alt="" style="width: 100%"></div> <div class="TextBox"><b>${tweet.name}</b><br>${tweet.message} <br>${tweet.tweetedAt}</div></li>`;
+        }
+    };
+
+    tweets.forEach(appendTweet);
+    tweetsHTML += "</ul>";
+    document.querySelector("#answer").innerHTML = tweetsHTML;
 
 }
+
+function onClickButtonTaro() {
+    let tweetsHTML ="<ul>";
+    let appendTweet = function (tweet) {
+        if(tweet.name === '太郎'){
+            tweetsHTML +=  `<li><div class="ImgBox"><img src=${tweet.avatar} alt="" style="width: 100%"></div> <div class="TextBox"><b>${tweet.name}</b><br>${tweet.message} <br>${tweet.tweetedAt}</div></li>`;
+        }
+    };
+    tweets.forEach(appendTweet);
+    tweetsHTML += "</ul>";
+    document.querySelector("#answer").innerHTML = tweetsHTML;
+}
+
+
