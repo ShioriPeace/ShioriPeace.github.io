d4e6208f0c7ece86a594e50885cd63e6eade1f77
ShioriPeace <m0116225f2@edu.teu.ac.jp>
2018-12-19 18:04:41 +0900

    シートとの連結完了（横に表示されない）

---
+++ M0116225_Final.js
@@ -0,0 +1,76 @@
+/*fetch("https://sheets.googleapis.com/v4/spreadsheets/1vvNBYDw8iaPidm7Z4-UtGLY8-Y2Yf5hzu2v5v7m1vHA/values/sheet1!A1:C6?key=AIzaSyDGu3-jXAEgxc9ywdeeFWXw9e8QGE3uk2c").then(
+    response => {
+        return response.json();
+    }
+).*/
+
+fetch("https://sheets.googleapis.com/v4/spreadsheets/1bULwtvSec8ZKUiJZ8qvPVgKpX0wGKKQXk7wekoP4lbo/values/data!A2:E26?key=AIzaSyCpc-46ZfrDixQdHk51nPde4gJlA06tcD8").then(
+    response => {
+        return response.json();
+    }
+).then(
+    json => {
+        /*let html = "<ul>";
+        console.log(json);
+        //ここに何か書く
+
+
+       json.values.forEach(row =>{ //jsonの中のvalueというところに入っているのを全て取り出す→その中のrowという配列の中を取り出す
+            html +=  `<li><${row[0]},${row[1]},${row[2]}</li>`
+        });//ここはfunction row
+
+        html = "</ul>";*/
+
+       /* let html = `    <div class="col-4">
+      <div class="card mx-auto" style="width: 18rem;">
+        <img class="card-img-top" src="octnalAudio.png" alt="カードの画像">
+        <div class="card-body">
+          <h5 class="card-title">OCTNAL AUDIO TABLE</h5>
+          <p class="card-text">CDを机に置くだけで音楽プレーヤーのように再生できるIoTデバイス</p>
+          <a href="#" class="btn btn-primary">詳しくはこちら</a>
+        </div>
+      </div>
+    </div>`;*/
+
+        let JsonCard = "<ul>";
+        json.values.forEach(row => {
+            JsonCard += `<div class="col-4">
+      <div class="card mx-auto" style="width: 18rem;">
+        <img class="card-img-top" src= "http://${row[3]}" alt="カードの画像">
+        <div class="card-body">
+          <h5 class="card-title">${row[0]}</h5>
+          <p class="card-text">${row[1]}</p>
+          <a href="#" class="btn btn-primary">詳しくはこちら</a>
+        </div>
+      </div>
+    </div>`;
+
+           // JsonCard += `<li><div class="row"><div class="col-4"><div class="card mx-auto" style="width: 18rem;"><div class="card-img-top"><img src="http://${row[3]}" alt="">
+            // <div class="card-title">${row[0]}</div></div></div></div></li>`
+        });
+
+        JsonCard += "</ul>";
+        document.querySelector("#contents").innerHTML = JsonCard;
+    }
+);
+
+/*let card = `<div class="col-4">
+      <div class="card mx-auto" style="width: 18rem;">
+        <img class="card-img-top" src="iroiro_320.jpg" alt="カードの画像">
+        <div class="card-body">
+          <h5 class="card-title">いろ色メモリアル</h5>
+          <p class="card-text">カメラで写したモノの色で絵が描けるAndroidアプリ</p>
+          <a href="#" class="btn btn-primary">詳しくはこちら</a>
+        </div>
+      </div>
+    </div>`;*/
+
+// function onClickTest() {
+//     document.querySelector("#contents").innerHTML += card;
+//
+// }?
+
+
+
+
+
--- sheets-sample.js
+++ sheets-sample.js
@@ -1,10 +1,4 @@
-/*fetch("https://sheets.googleapis.com/v4/spreadsheets/1vvNBYDw8iaPidm7Z4-UtGLY8-Y2Yf5hzu2v5v7m1vHA/values/sheet1!A1:C6?key=AIzaSyDGu3-jXAEgxc9ywdeeFWXw9e8QGE3uk2c").then(
-    response => {
-        return response.json();
-    }
-).*/
-
-fetch("https://sheets.googleapis.com/v4/spreadsheets/1bULwtvSec8ZKUiJZ8qvPVgKpX0wGKKQXk7wekoP4lbo/values/data?key=AIzaSyAHmMpbMqePjuQXzThfIjZ4zAFkwaFHxTE").then(
+fetch("https://sheets.googleapis.com/v4/spreadsheets/1bULwtvSec8ZKUiJZ8qvPVgKpX0wGKKQXk7wekoP4lbo/values/data?key=AIzaSyCpc-46ZfrDixQdHk51nPde4gJlA06tcD8").then(
     response => {
         return response.json();
     }
@@ -12,13 +6,13 @@ fetch("https://sheets.googleapis.com/v4/spreadsheets/1bULwtvSec8ZKUiJZ8qvPVgKpX0
     json => {
         let html = "<ul>";
         console.log(json);
-        //ここに何か書く
 
-        json.values.forEach(row =>{ //jsonの中のvalueというところに入っているのを全て取り出す→その中のrowという配列の中を取り出す
-          html +=  `<li><${row[0]},${row[1]}.${row[2]}</li>`
-        });//ここはfunction row
+        json.values.forEach( row => {
+                html += `<li>${row[0]}, ${row[1]}, ${row[2]}</li>`;
+            }
+        );
 
-        html = "</ul>";
+        html += "</ul>";
         document.querySelector("#result").innerHTML = html;
     }
 );
